289d53fe3d013f8d16da148beff1a91f42c16be1
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-12-30 21:56:34 +0000

    Set up angular app and controller

---
+++ js/app/controllers/MainController.js
@@ -0,0 +1,7 @@
+function MainController($scope) {
+  $scope.name = "Khaya"
+  $scope.email = "khaya@gmail.com"
+  $scope.phone = "2018338333"
+}
+
+angular.module('app').controller('MainController', MainController)
